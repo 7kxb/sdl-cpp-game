827b3ebc82390cf4f93e07d6078c3c378342e168
7kxb <kiwi123food@gmail.com>
2023-06-02 14:21:03 -0400

    idk

--- test.cpp
+++ test.cpp
@@ -1,10 +1,25 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 using namespace std;
+SDL_Window *window = SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+class player {
+    public:
+        int x = 200;
+        int y = 300;
+        int s = 10;
+        void show () {
+            SDL_Rect rect;
+            rect.x = player().x;
+            rect.y = player().y;
+            rect.w = player().s;
+            rect.h = player().s;
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderDrawRect(renderer, &rect);
+        }
+};
 int main (int argv, char** args) {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     bool isRunning = true;
     SDL_Event event;
     while (isRunning) {
@@ -21,6 +36,7 @@ int main (int argv, char** args) {
         }
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        player().show();
         SDL_RenderPresent(renderer);
     }
     SDL_DestroyRenderer(renderer);
